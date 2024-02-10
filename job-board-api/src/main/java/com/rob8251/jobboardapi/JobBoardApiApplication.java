9f7b215d756dfcaf6ea16c5711565c3dfe7a9ecd
rutrob <faherlele@gmail.com>
2024-02-10 16:41:29 +0100

    Initial commit

---
+++ job-board-api/src/main/java/com/rob8251/jobboardapi/JobBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.rob8251.jobboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobBoardApiApplication.class, args);
+    }
+
+}
